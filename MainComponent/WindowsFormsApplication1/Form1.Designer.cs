1ab069533f4e4c7b6e963021915af8f9ae69ecde
DeadDodoRS <imports@mail.ru>
2017-12-08 20:07:18 +0400

    Sync

--- MainComponent/WindowsFormsApplication1/Form1.Designer.cs
+++ MainComponent/WindowsFormsApplication1/Form1.Designer.cs
@@ -29,42 +29,53 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.mainComponent1 = new MainComp.MainComponent();
+            this.mainComponent2 = new MainComp.MainComponent();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // mainComponent1
+            // mainComponent2
             // 
-            this.mainComponent1.BackColor = System.Drawing.SystemColors.Control;
-            this.mainComponent1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("mainComponent1.BackgroundImage")));
-            this.mainComponent1.BackgroundImagePrimary = ((System.Drawing.Image)(resources.GetObject("mainComponent1.BackgroundImagePrimary")));
-            this.mainComponent1.CaptchaPattern = MainComp.MainComponent.TypesOfImages.Face;
-            this.mainComponent1.ColorLine = System.Drawing.Color.Blue;
-            this.mainComponent1.CountCorrectChild = 3;
-            this.mainComponent1.CountNonCorrectChild = 3;
-            this.mainComponent1.ErrorNumber = 3;
-            this.mainComponent1.Location = new System.Drawing.Point(24, 25);
-            this.mainComponent1.MaximumSize = new System.Drawing.Size(400, 180);
-            this.mainComponent1.MinimumSize = new System.Drawing.Size(400, 180);
-            this.mainComponent1.Name = "mainComponent1";
-            this.mainComponent1.PathWrongChildPicture = "C:\\Users\\Sergey\\Documents\\PictureForCaptcha\\1";
-            this.mainComponent1.PathRightChildPicture = "";
-            this.mainComponent1.RandomLocationChild = false;
-            this.mainComponent1.Size = new System.Drawing.Size(400, 180);
-            this.mainComponent1.TabIndex = 0;
-            this.mainComponent1.TextHelp = "Text\r\nhelp";
+            this.mainComponent2.BackColor = System.Drawing.SystemColors.Control;
+            this.mainComponent2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("mainComponent2.BackgroundImage")));
+            this.mainComponent2.BackgroundImagePrimary = ((System.Drawing.Image)(resources.GetObject("mainComponent2.BackgroundImagePrimary")));
+            this.mainComponent2.CaptchaPattern = TypesOfImages.Face;
+            this.mainComponent2.ColorLine = System.Drawing.Color.Blue;
+            this.mainComponent2.CountCorrectChild = 3;
+            this.mainComponent2.CountNonCorrectChild = 3;
+            this.mainComponent2.ErrorNumber = 3;
+            this.mainComponent2.Location = new System.Drawing.Point(12, 12);
+            this.mainComponent2.MaximumSize = new System.Drawing.Size(400, 180);
+            this.mainComponent2.MinimumSize = new System.Drawing.Size(400, 180);
+            this.mainComponent2.Name = "mainComponent2";
+            this.mainComponent2.PathRightChildPicture = "";
+            this.mainComponent2.PathWrongChildPicture = "";
+            this.mainComponent2.RandomLocationChild = false;
+            this.mainComponent2.Size = new System.Drawing.Size(400, 180);
+            this.mainComponent2.TabIndex = 0;
+            this.mainComponent2.TextHelp = "Text\r\nhelp";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(445, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(637, 308);
+            this.ClientSize = new System.Drawing.Size(673, 299);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.mainComponent2);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private MainComp.MainComponent mainComponent1;
+        private MainComp.MainComponent mainComponent2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
